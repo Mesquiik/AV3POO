2ab6d0952de99125770abe340680c23e9886c405
Átila Leite <162616250+atilario@users.noreply.github.com>
2025-05-28 16:36:16 -0300

    Update App.java

--- main/App.java
+++ main/App.java
@@ -15,10 +15,9 @@ public class App{
     System.out.println("1. Ver todos os jogos cadastrados?");
     System.out.println("2. Cadastrar um novo jogo?");
     System.out.println("3. Remover algum jogo existente?");
-    System.out.println("4. Ver os jogos em ordem alfabética?");
-    System.out.println("5. Procurar um jogo pelo código?");
-    System.out.println("6. Alterar os dados de um jogo?");
-    System.out.println("7. Sair do sistema?");
+    System.out.println("4. Procurar um jogo pelo código?");
+    System.out.println("5. Alterar os dados de um jogo?");
+    System.out.println("6. Sair do sistema?");
     System.out.print("Digite a opção desejada: ");
     resposta = scanner.nextLine();
 
@@ -36,18 +35,14 @@ public class App{
                   break;
         
               case "4":
-                  // chamar função ordenarPorNome();
-                  break;
-        
-              case "5":
                  // chamar função buscarJogoPorCodigo();
                   break;
         
-              case "6":
+              case "5":
                   // chamar função alterarJogo();
                   break;
         
-              case "7":
+              case "6":
                   System.out.println("Encerrando o sistema. Até logo!");
                   break;
         
