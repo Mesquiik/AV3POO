246d375fcd5234350acd8465994b31719e39137e
Átila Leite <162616250+atilario@users.noreply.github.com>
2025-05-28 16:16:32 -0300

    Update JogoDado.java

--- jogos/JogoDado.java
+++ jogos/JogoDado.java
@@ -2,6 +2,6 @@ package jogos;
 
 public class JogoDado extends Jogo{
   public JogoDado (String nome, String codigo){
-  super (nome, codigo, "Dado");
+  super (nome, codigo, "Dado", preco);
   }
 }
