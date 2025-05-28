3ffec129fb36a7d943b58a32f287ed36ebac17bb
Átila Leite <162616250+atilario@users.noreply.github.com>
2025-05-28 16:02:41 -0300

    Update Jogos.java

--- jogos/Jogos.java
+++ jogos/Jogos.java
@@ -1,1 +1,24 @@
+packages jogos;
 
+public abstract class Jogo {
+  private String nome;
+  private string codigo;
+  private String categoria:
+
+public Jogo (Sring nome, String codigo, String categoria){
+  this.nome = nome;
+  this.codigo = codigo;
+  this.categoria = categoria;
+}
+
+public String getNome() {
+        return nome;
+    }
+public String getCodigo() {
+        return codigo;
+}
+
+public String getCategoria() {
+        return categoria;
+}
+}
