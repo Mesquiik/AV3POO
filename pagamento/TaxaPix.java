1a941d4a1813fa91bd20d66f8158dd249f64cc7e
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-05-28 16:39:29 -0300

    Update TaxaPix.java

--- pagamento/TaxaPix.java
+++ pagamento/TaxaPix.java
@@ -1,1 +1,14 @@
+package pagamento;
+
+public class TaxaPix implements TaxaPagamento {
+
+	@Override
+	public Double calculaTaxa(Double valor) {
+		
+		Double taxa;
+		taxa = valor*(-0.1); //10% de desconto (vai retornar negativo para reduzir o preço)
+		return taxa;
+	}
+
+}
 
