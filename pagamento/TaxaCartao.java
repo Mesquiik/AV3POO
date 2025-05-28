7d041b7aa77a1071f757c49870651954602b5c3f
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-05-28 16:38:46 -0300

    Update TaxaCartao.java

--- pagamento/TaxaCartao.java
+++ pagamento/TaxaCartao.java
@@ -1,1 +1,14 @@
+package pagamento;
+
+public class TaxaCartao implements TaxaPagamento {
+
+	@Override
+	public Double calculaTaxa(Double valor) {
+		
+		Double taxa;
+		taxa = valor*(0.05); //5% de acrescimo
+		return taxa;
+	}
+
+}
 
