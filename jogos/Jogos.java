7ee2b23953bf5396a968a707263e5219e4cf8017
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-05-28 17:49:30 -0300

    Update Jogos.java

--- jogos/Jogos.java
+++ jogos/Jogos.java
@@ -1,18 +1,58 @@
-package main;
-import jogos.*;
-import manipulacao.*;
-import pagamento.*;
-import java.util.*;
+package jogos;
 
-public class App{
-  public static void main(String[] args) {
-		
-		
-		FuncoesMenu menu = new FuncoesMenu();
-		
-		menu.cadastrarJogosIniciais();
-		
-		menu.menuLoop();
-    
-  }
+public abstract class Jogos {
+  private String nome;
+  private String codigo;
+  private String categoria;
+  private Double preco;
+
+public Jogos (String nome, String codigo, String categoria, Double preco){
+  this.nome = nome;
+  this.codigo = codigo;
+  this.categoria = categoria;
+  this.preco = preco;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public String getCodigo() {
+	return codigo;
+}
+
+public void setCodigo(String codigo) {
+	this.codigo = codigo;
+}
+
+public String getCategoria() {
+	return categoria;
+}
+
+public void setCategoria(String categoria) {
+	this.categoria = categoria;
+}
+
+public Double getPreco() {
+	return preco;
+}
+
+public void setPreco(Double preco) {
+	this.preco = preco;
+}
+
+@Override
+public String toString() {
+	return "Jogos [nome=" + nome + ", codigo=" + codigo + ", categoria=" + categoria + ", preco=" + preco + "]";
+}
+
+
+
+
+
+
 }
