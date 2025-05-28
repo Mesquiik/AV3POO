fd8ae875cf1dd9a9830152f98ed3dd3f9307b4f0
Átila Leite <162616250+atilario@users.noreply.github.com>
2025-05-28 16:17:39 -0300

    Update JogoCarta.java

--- jogos/JogoCarta.java
+++ jogos/JogoCarta.java
@@ -1,7 +1,7 @@
 package jogos;
 
 public class JogoCarta extends Jogo{
-  public JogoCarta (String nome, String codigo){
+  public JogoCarta (String nome, String codigo, Double preco){
     super(nome, codigo, "Carta", preco);
   }
 }
