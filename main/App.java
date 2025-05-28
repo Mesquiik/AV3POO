6ed4f6858e59ab431186d1d2f44885be4f235f90
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-05-28 18:14:47 -0300

    Update App.java

--- main/App.java
+++ main/App.java
@@ -7,7 +7,10 @@ import java.util.*;
 public class App{
   public static void main(String[] args) {
 		
-		
+		TaxaLoja taxaBarra = new TaxaBarra();   //Perguntar antes de tudo
+		TaxaLoja taxaSalvador = new TaxaSalvador();
+
+	  
 		FuncoesMenu menu = new FuncoesMenu();
 		
 		menu.cadastrarJogosIniciais();
