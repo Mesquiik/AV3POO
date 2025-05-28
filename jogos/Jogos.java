5a0e2f1d9c434d4719c9f7dd496671378f3d8b3a
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-05-28 17:09:14 -0300

    Update Jogos.java

--- jogos/Jogos.java
+++ jogos/Jogos.java
@@ -1,12 +1,12 @@
-packages jogos;
+package jogos;
 
 public abstract class Jogo {
   private String nome;
-  private string codigo;
+  private String codigo;
   private String categoria;
   private Double preco;
 
-public Jogo (Sring nome, String codigo, String categoria, Double preco){
+public Jogo (String nome, String codigo, String categoria, Double preco){
   this.nome = nome;
   this.codigo = codigo;
   this.categoria = categoria;
