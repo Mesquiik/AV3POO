c859f1350971643e8dddfd5edf1277dc3cfe9cb5
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-05-28 16:37:48 -0300

    Update ProcessarPagamento.java

--- pagamento/ProcessarPagamento.java
+++ pagamento/ProcessarPagamento.java
@@ -1,1 +1,34 @@
+package pagamento;
 
+public class ProcessarPagamento {
+
+	private Double valorJogo;
+	private TaxaLoja taxaLoja;
+	private TaxaPagamento taxaPagamento;
+	private Integer dias;
+	
+	public ProcessarPagamento(Double valorJogo, TaxaLoja taxaLoja, TaxaPagamento taxaPagamento, Integer dias) {		//MEXER NOS DIAS, TALVEZ COLOCAR DATA
+		this.valorJogo = valorJogo;
+		this.taxaLoja = taxaLoja;
+		this.taxaPagamento = taxaPagamento;
+		this.dias = dias;
+	}
+	
+	
+	public Double calculaValorFinal() {
+		
+		Double valor;
+		
+		valor = valorJogo*dias;
+		valor += taxaLoja.calculaTaxa();
+		valor += taxaPagamento.calculaTaxa(valor);
+		
+		return valor;
+		
+	}
+
+	
+	
+	
+	
+}
