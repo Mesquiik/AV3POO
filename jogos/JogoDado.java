ece32554b9040fde157b54b02cffd8d795b75b45
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-05-28 17:10:57 -0300

    Update JogoDado.java

--- jogos/JogoDado.java
+++ jogos/JogoDado.java
@@ -1,6 +1,6 @@
 package jogos;
 
-public class JogoDado extends Jogo{
+public class JogoDado extends Jogos{
   public JogoDado (String nome, String codigo, Double preco){
   super (nome, codigo, "Dado", preco);
   }
