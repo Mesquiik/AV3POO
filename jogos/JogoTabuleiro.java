5ecd80760f111e2dafc698061ae408ccaafa3185
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-05-28 17:11:04 -0300

    Update JogoTabuleiro.java

--- jogos/JogoTabuleiro.java
+++ jogos/JogoTabuleiro.java
@@ -1,6 +1,6 @@
 package jogos;
 
-public class JogoTabuleiro extends Jogo {
+public class JogoTabuleiro extends Jogos {
   public JogoTabuleiro (String nome, String codigo, Double preco){
     super (nome, codigo, "Tabuleiro", preco);
   }
