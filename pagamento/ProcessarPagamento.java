f986743d6d5ecdd0d6357510c0dd597c7ed8d889
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-06-02 15:37:16 -0300

    Update ProcessarPagamento.java

--- pagamento/ProcessarPagamento.java
+++ pagamento/ProcessarPagamento.java
@@ -1,40 +1,39 @@
-	package pagamento;
-	import java.util.Scanner;
-	
-	public class ProcessarPagamento {
+package pagamento;
 
-		public ProcessarPagamento() {		
-			
-		}
-		
+import java.util.Scanner;
 
-		public TaxaLoja unidadeLoja() { 
-			
-			Scanner sc = new Scanner(System.in);
-			
-			TaxaLoja taxaSalvador = new TaxaSalvador();
-			TaxaLoja taxaBarra = new TaxaBarra();
-			
-			Integer escolha = 0;
-			
-			while(escolha != 1 && escolha != 2) {
-				System.out.println("Qual a unidade?");
-				System.out.println("1- Shopping Barra");
-				System.out.println("2- Shopping Salvador");
-				
-				escolha = sc.nextInt();
-				
-				if(escolha != 1 && escolha != 2) {
-					 System.out.println("Escolha inválida, tente novamente");
-				}
-			}
-			
-			if(escolha == 1) {
-				return taxaSalvador;
-			}else {
-				return taxaBarra;
-			}
-			
-			
-		}
+public class ProcessarPagamento {
+
+	public ProcessarPagamento() {
+
 	}
+
+	public TaxaLoja unidadeLoja() {
+
+		Scanner sc = new Scanner(System.in);
+
+		TaxaLoja taxaSalvador = new TaxaSalvador();
+		TaxaLoja taxaBarra = new TaxaBarra();
+
+		Integer escolha = 0;
+
+		while (escolha != 1 && escolha != 2) {
+			System.out.println("Qual a unidade?");
+			System.out.println("1- Shopping Barra");
+			System.out.println("2- Shopping Salvador");
+
+			escolha = sc.nextInt();
+
+			if (escolha != 1 && escolha != 2) {
+				System.out.println("Escolha inválida, tente novamente");
+			}
+		}
+
+		if (escolha == 1) {
+			return taxaSalvador;
+		} else {
+			return taxaBarra;
+		}
+
+	}
+}
