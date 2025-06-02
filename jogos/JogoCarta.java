9d3173e64946b9576e929c5f670ee07976818585
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-06-02 15:36:02 -0300

    Update JogoCarta.java

--- jogos/JogoCarta.java
+++ jogos/JogoCarta.java
@@ -1,16 +1,16 @@
 package jogos;
 
-public class JogoCarta extends Jogos{
-	
+public class JogoCarta extends Jogos {
+
 	private Integer numeroCartas;
-	
-  public JogoCarta (String nome, String codigo, Double preco, Integer numeroCartas){
-    super(nome, codigo, "Carta", preco);
-    this.numeroCartas = numeroCartas;
-  }
-  
-  @Override
-  public String toString() {
-	  return super.toString() + " Número de cartas: " + numeroCartas;
-  }
+
+	public JogoCarta(String nome, String codigo, Double preco, Integer numeroCartas) {
+		super(nome, codigo, "Carta", preco);
+		this.numeroCartas = numeroCartas;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " Número de cartas: " + numeroCartas;
+	}
 }
