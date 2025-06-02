bdc5fd62410b7d4a0f5fa8150e41007cefa17110
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-06-02 15:35:49 -0300

    Update Jogos.java

--- jogos/Jogos.java
+++ jogos/Jogos.java
@@ -1,54 +1,53 @@
 package jogos;
 
 public abstract class Jogos {
-  private String nome;
-  private String codigo;
-  private String categoria;
-  private Double preco;
+	private String nome;
+	private String codigo;
+	private String categoria;
+	private Double preco;
 
-public Jogos (String nome, String codigo, String categoria, Double preco){
-  this.nome = nome;
-  this.codigo = codigo;
-  this.categoria = categoria;
-  this.preco = preco;
-}
+	public Jogos(String nome, String codigo, String categoria, Double preco) {
+		this.nome = nome;
+		this.codigo = codigo;
+		this.categoria = categoria;
+		this.preco = preco;
+	}
 
-public String getNome() {
-	return nome;
-}
+	public String getNome() {
+		return nome;
+	}
 
-public void setNome(String nome) {
-	this.nome = nome;
-}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-public String getCodigo() {
-	return codigo;
-}
+	public String getCodigo() {
+		return codigo;
+	}
 
-public void setCodigo(String codigo) {
-	this.codigo = codigo;
-}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
 
-public String getCategoria() {
-	return categoria;
-}
+	public String getCategoria() {
+		return categoria;
+	}
 
-public void setCategoria(String categoria) {
-	this.categoria = categoria;
-}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
 
-public Double getPreco() {
-	return preco;
-}
+	public Double getPreco() {
+		return preco;
+	}
 
-public void setPreco(Double preco) {
-	this.preco = preco;
-}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
 
-@Override
-public String toString() {
-	return "Nome: " + nome + ", Código: " + codigo + ", Categoria: " + categoria + ", Preço/dia: R$" + preco + ",";
-}
-
+	@Override
+	public String toString() {
+		return "Nome: " + nome + ", Código: " + codigo + ", Categoria: " + categoria + ", Preço/dia: R$" + preco + ",";
+	}
 
 }
