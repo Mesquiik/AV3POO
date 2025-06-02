53f45b25ddda4810a6509b738533656337ff4b95
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-06-02 15:37:34 -0300

    Update TaxaPix.java

--- pagamento/TaxaPix.java
+++ pagamento/TaxaPix.java
@@ -6,7 +6,7 @@ public class TaxaPix implements TaxaPagamento {
 	public Double calculaTaxa(Double valor) {
 		
 		Double taxa;
-		taxa = valor*(-0.1); //10% de desconto (vai retornar negativo para reduzir o preço)
+		taxa = valor * (-0.1); //10% de desconto (vai retornar negativo para reduzir o preço)
 		return taxa;
 	}
 
