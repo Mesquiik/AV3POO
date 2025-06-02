71880570e7a105bda350935849e9fdaa98117939
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-06-02 15:36:31 -0300

    Update App.java

--- main/App.java
+++ main/App.java
@@ -1,21 +1,15 @@
 package main;
-import jogos.*;
+
 import manipulacao.*;
-import pagamento.*;
-import java.util.*;
 
-public class App{
-  public static void main(String[] args) {
-		
-		TaxaLoja taxaBarra = new TaxaBarra();   //Perguntar antes de tudo
-		TaxaLoja taxaSalvador = new TaxaSalvador();
+public class App {
+	public static void main(String[] args) {
 
-	  
 		FuncoesMenu menu = new FuncoesMenu();
-		
+
 		menu.cadastrarJogosIniciais();
-		
+
 		menu.menuLoop();
-    
-  }
+
+	}
 }
