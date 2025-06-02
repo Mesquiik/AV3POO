f454262fdd5f5aa39e26c6c9c757c590855b0f9c
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-06-02 15:36:12 -0300

    Update JogoDado.java

--- jogos/JogoDado.java
+++ jogos/JogoDado.java
@@ -1,16 +1,16 @@
 package jogos;
 
-public class JogoDado extends Jogos{
-	
+public class JogoDado extends Jogos {
+
 	private Integer numeroDados;
-	
-  public JogoDado (String nome, String codigo, Double preco, Integer numeroDados){
-	  super (nome, codigo, "Dado", preco);
-	  this.numeroDados = numeroDados;
-  }
-  
-  @Override
-  public String toString() {
-	  return super.toString() + " Número de dados: " + numeroDados;
-  }
+
+	public JogoDado(String nome, String codigo, Double preco, Integer numeroDados) {
+		super(nome, codigo, "Dado", preco);
+		this.numeroDados = numeroDados;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " Número de dados: " + numeroDados;
+	}
 }
