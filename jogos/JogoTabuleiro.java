94275f40060d731bc6308fd9c8156783db94b597
Heitor Barreto <162578018+Heitorab06@users.noreply.github.com>
2025-06-02 15:36:21 -0300

    Update JogoTabuleiro.java

--- jogos/JogoTabuleiro.java
+++ jogos/JogoTabuleiro.java
@@ -1,15 +1,16 @@
 package jogos;
 
 public class JogoTabuleiro extends Jogos {
-	
+
 	private Integer numeroPecas;
-	public JogoTabuleiro (String nome, String codigo, Double preco, Integer numeroPecas){
-    	super (nome, codigo, "Tabuleiro", preco);
-    	this.numeroPecas = numeroPecas;
-  }
-	
+
+	public JogoTabuleiro(String nome, String codigo, Double preco, Integer numeroPecas) {
+		super(nome, codigo, "Tabuleiro", preco);
+		this.numeroPecas = numeroPecas;
+	}
+
 	@Override
-	  public String toString() {
-		  return super.toString() + " Número de peças: " + numeroPecas;
-	  }
+	public String toString() {
+		return super.toString() + " Número de peças: " + numeroPecas;
+	}
 }
